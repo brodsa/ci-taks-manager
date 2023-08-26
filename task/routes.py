f9e734dae932df9d2360807bb6bceeb815f02499
brodsa <brodinova@gmail.com>
2023-08-26 15:08:34 +0000

    feat: create task page using materialize and templating

--- task/routes.py
+++ task/routes.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from task import app, db
+from task.models import Category, Task
 
 
 @app.route("/")
 def home():
-    return render_template("base.html")
+    return render_template("tasks.html")
