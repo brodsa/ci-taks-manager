0e4674e863ff6773ecf098ff9a3d54eb7b9604e2
brodsa <brodinova@gmail.com>
2023-08-28 16:00:23 +0000

    feat: store the add category

--- task/routes.py
+++ task/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from task import app, db
 from task.models import Category, Task
 
@@ -6,3 +6,19 @@ from task.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        print(category)
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
